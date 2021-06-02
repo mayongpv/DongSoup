63bfd77d244dd4addf724f655ba6fbecfb4d37f6
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-02 21:21:08 +0900

    Shader 만들기

--- Assets/MyCube.cs
+++ Assets/MyCube.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MyCube : MonoBehaviour
 {
@@ -10,7 +8,23 @@ public class MyCube : MonoBehaviour
         rotate.enabled = !rotate.enabled;
     }
 
- 
+
+    private object i;
+    public void Update()
+
+    {
+        for (int i = 0; i < 10; i++) 
+
+        {
+
+
+
+            Debug.Log(i);
+
+        }
+
+
+}
 }
 //쓰기 완료
 
