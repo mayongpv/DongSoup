06aa42e5cff7e71c00c4819c2707ee7cbb0cfd35
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-02 17:19:45 +0900

    수정 전

---
+++ Assets/MyCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCube : MonoBehaviour
+{
+    public void OnClickMessage()
+    {
+        var rotate = GetComponent<GORotate>();
+        rotate.enabled = !rotate.enabled;
+    }
+
+ 
+}
+//쓰기 완료
+
+
--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -1,18 +1,26 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 3D오브젝트 클릭 이벤트 
+    private void OnMouseDown()
     {
-        
+        print(transform.GetPath() + "OnClickMessage 실행");
+        //플레이어가 나에게 들어와 있는 방식인가?
+        //2가지 방법으로 확인 가능
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        //부딪히면 나뭇잎을 흔들어라.
+        if (other.CompareTag("Player"))
+        {
+            Animator[] animators = GetComponentsInChildren<Animator>();
+            for (int i = 0; i < animators.Length; i++)
+            {
+                animators[i].Play("Shake", 0, 0); 
+            }
+        }
     }
 }
