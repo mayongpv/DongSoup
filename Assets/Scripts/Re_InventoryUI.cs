fbea86e9c97a10468af44baa210ca8ed5b174a96
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-13 23:29:40 +0900

    잉?

---
+++ Assets/Scripts/Re_InventoryUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+using UnityEngine;
+
+
+public class Re_InventoryUI : MonoBehaviour
+{
+    public Transform blankIcon;
+    //비어있는 슬롯 위치 아이콘을 10개씩 4줄로 배치하자.//블랭크 아이콘을 배치
+
+    public List<GameObject> = new List<GameObject>
+    [ContextMenu("아이콘 생성")]
+
+    void Start()
+    {
+        posItems.ForEach(x => Destroy(x));
+
+        int xCount = 10;
+        int yCount = 4;
+        Vector3 origialPos = blankIcon.localPosition;
+        for (int y = 0; y < yCount; y++)
+        {
+            for (int x = 0; x < xCount; x++)
+            {
+                var newPos = origialPos;
+                newPos.x += x * xSize;
+                newPos.y += y * ySize;
+                GameObject newGO = Instantiate(blankIcon.gameObject);
+                newGO.transform.SetParent(transform);
+                newGO.transform.localPosition = newPos;
+                posItems.Add(newGO);
+            }
+        }
+ 
+
+    }
+
+    void Update()
+    {
+
+    }
+}
